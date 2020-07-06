8a4492542e1a3eb9409d1b7e8a7bbc22d01674ba
PrenitaVulpo <pingu@localhost.localdomain>
2020-07-05 22:25:35 -0300

    desafio 2 resolvido

--- desafio2/desafio2.js
+++ desafio2/desafio2.js
@@ -1,4 +1,21 @@
-#Desafio2-  Conversor de números romanos para decimais
+//#Desafio2-  Conversor de números romanos para decimais
 
 const rl = require('readline-sync');
 
+function Start() {
+    num = rl.question('informe um número a ser convertido: ');
+    arrayResultado = [];
+    algarismosRomanos = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    decimais = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    resto = num;
+    for (x=12;x>0;x--) {
+        casa = 12 - x;
+        contador = Math.floor(resto / decimais[casa]);
+        for (z = contador; z > 0; z--) {
+            arrayResultado.push(algarismosRomanos[casa]);
+        };
+        resto = resto-(contador*decimais[casa]);
+    };
+    console.log(arrayResultado.join(''));
+}
+Start()
